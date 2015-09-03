efc680e1fa36f8215f93c0294a0dd709ab1a61d7
Pratyush <pratyush@techmojo.in>
2015-09-03 13:19:27 +0530

    Changes in url

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,7 @@ require(['socket.io','socket.io-stream'],function(socketIO,SocketStream){
 	})
 	clearHTMLElements();
 	function createSocket(){
-		socket = socketIO.connect('http://localhost:8082');
+		socket = socketIO.connect('https://warm-crag-7047.herokuapp.com:53056');
 	}
 	createSocket();
 	function sendFile(file){
