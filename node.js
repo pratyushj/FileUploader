e0286098c9a004e226936ee5a90e1c8777433622
Pratyush <pratyush@techmojo.in>
2015-09-03 14:33:53 +0530

    Changes for upload folder

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-	app = express(),
+app = express(),
 server = require('http').Server(app);
 var io = require('socket.io')(server),
 socketStream =require('socket.io-stream'),
@@ -7,38 +7,42 @@ fs = require('fs');
 var path = require('path');
 var port = process.env.PORT||8082;
 app.use(express.static(__dirname));
-
+var uploadRelPath="Uploads/";
 var server1 =server.listen(port,function(){
-    console.log("server Listening on port "+port );
+	console.log("server Listening on port "+port );
 });
 // app.listen(port,function(){
 //     console.log('starting on  : '+ port);
 // });
 
 io.on('connection', function(socket) {
-  socketStream(socket).on('fileUpload', function(stream, data) {
-    var filename = path.basename(data.name);
-    console.log(data);
-    // console.log(stream);
-    stream.pipe(fs.createWriteStream("Uploads/"+filename));
-  });
-  socket.on('fileClear',function(){
-  	removeFiles("Uploads/");
-  })
+	socketStream(socket).on('fileUpload', function(stream, data) {
+		var filename = path.basename(data.name);
+		console.log(data);
+		fs.lstatSync(uploadRelPath, function(err, stats) {
+			if (!err && stats.isDirectory()) {
+				fs.mkdir(uploadRelPath);
+			}
+		});
+		stream.pipe(fs.createWriteStream(uploadRelPath+filename));
+	});
+	socket.on('fileClear',function(){
+		removeFiles(uploadRelPath);
+	})
 });
 function removeFiles(dirPath) {
-      try { var files = fs.readdirSync(dirPath); }
-      catch(e) { 
-      	console.log(JSON.stringify(e));
-      	return;
-      	 }
-      console.log("Removing All Files"+JSON.stringify(files));
-      if (files.length > 0)
-        for (var i = 0; i < files.length; i++) {
-          var filePath = dirPath + '/' + files[i];
-          if (fs.statSync(filePath).isFile())
-            fs.unlinkSync(filePath);
-          else
-            rmDir(filePath);
-        }
-    };
+	try { var files = fs.readdirSync(dirPath); }
+	catch(e) { 
+		console.log(JSON.stringify(e));
+		return;
+	}
+	console.log("Removing All Files"+JSON.stringify(files));
+	if (files.length > 0)
+		for (var i = 0; i < files.length; i++) {
+			var filePath = dirPath + '/' + files[i];
+			if (fs.statSync(filePath).isFile())
+				fs.unlinkSync(filePath);
+			else
+				rmDir(filePath);
+		}
+	};
